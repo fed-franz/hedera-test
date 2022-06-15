8a3d213b9e2fc00c16e92a12a6908c43d44e9e3d
FedFranz <fed-franz@users.noreply.github.com>
2022-06-15 17:23:27 +0200

    rename init

--- init.js
+++ init.js
@@ -1,5 +1,15 @@
-const { Client, PrivateKey, AccountCreateTransaction, AccountBalanceQuery, Hbar, TransferTransaction} = require("@hashgraph/sdk");
+const {
+    Client
+} = require("@hashgraph/sdk");
+
 require("dotenv").config();
+
+const prompt = require('prompt-sync')();
+const yesno = require('yesno');
+
+const {setEnv} = require('./utils')
+const {createTopic, createAccount} = require('./lib-hedera');
+
 
 async function main() {
 
@@ -8,63 +18,45 @@ async function main() {
     const myPrivateKey = process.env.MY_PRIVATE_KEY;
 
     // If we weren't able to grab it, we should throw a new error
-    if (myAccountId == null ||
-        myPrivateKey == null ) {
+    if (myAccountId == null || myAccountId == '_' || myPrivateKey == null || myPrivateKey == '_' ) {
         throw new Error("Environment variables myAccountId and myPrivateKey must be present");
     }
 
     // Create our connection to the Hedera network
-    // The Hedera JS SDK makes this really easy!
     const client = Client.forTestnet();
-
     client.setOperator(myAccountId, myPrivateKey);
 
-    //Create new keys
-    const newAccountPrivateKey = await PrivateKey.generateED25519(); 
-    const newAccountPublicKey = newAccountPrivateKey.publicKey;
+    console.log("Hedera Nodes:");
+    console.log( client.network ); //TODO: print addresses only
+    console.log("Hedera Mirror:");
+    console.log( client.mirrorNetwork ); 
 
-    //Create a new account with 1,000 tinybar starting balance
-    const newAccountTransactionResponse = await new AccountCreateTransaction()
-        .setKey(newAccountPublicKey)
-        .setInitialBalance(Hbar.fromTinybars(1000))
-        .execute(client);
+    //Grab the topic ID from .env file, create one, or get from input
+    myTopicId = process.env.TOPIC_ID;
+    if (myTopicId == null || myTopicId == '_') {
+        const ok = await yesno({
+            question: 'TOPIC_ID not defined. Do you want to insert one?'
+        });
+        if (ok) 
+            myTopicId = prompt('Please insert the topic ID: ');    
+        else{
+            const ok2 = await yesno({
+                question: 'Do you want to create a new topic?'
+            });
+            if(ok2)
+                myTopicId = await createTopic(client);
+            else
+                console.log("WARNING: TOPIC_ID is not defined. Please insert it in the .env file or create a new one")
+                return
+        }
+            
+        setEnv("TOPIC_ID", myTopicId);
+    }
 
-    // Get the new account ID
-    const getReceipt = await newAccountTransactionResponse.getReceipt(client);
-    const newAccountId = getReceipt.accountId;
+    ///////////////////////////////////////////////////////////////////
 
-    console.log("The new account ID is: " +newAccountId);
-
-    //Verify the account balance
-    const accountBalance = await new AccountBalanceQuery()
-        .setAccountId(newAccountId)
-        .execute(client);
-
-    console.log("The new account balance is: " +accountBalance.hbars.toTinybars() +" tinybar.");
-
-    //Create the transfer transaction
-    const sendHbar = await new TransferTransaction()
-        .addHbarTransfer(myAccountId, Hbar.fromTinybars(-1000))
-        .addHbarTransfer(newAccountId, Hbar.fromTinybars(1000))
-        .execute(client);
-
-    //Verify the transaction reached consensus
-    const transactionReceipt = await sendHbar.getReceipt(client);
-    console.log("The transfer transaction from my account to the new account was: " + transactionReceipt.status.toString());
-    
-    //Request the cost of the query
-    const queryCost = await new AccountBalanceQuery()
-     .setAccountId(newAccountId)
-     .getCost(client);
-
-    console.log("The cost of query is: " +queryCost);
-
-    //Check the new account's balance
-    const getNewBalance = await new AccountBalanceQuery()
-        .setAccountId(newAccountId)
-        .execute(client);
-
-    console.log("The account balance after the transfer is: " +getNewBalance.hbars.toTinybars() +" tinybar.")
-
+    console.log("Account ID: "+myAccountId);
+    console.log("Topic ID: "+myTopicId);
 }
+
 main();
